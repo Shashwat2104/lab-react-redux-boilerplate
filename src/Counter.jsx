bbc94e16b63373f7061def56eff362d79b02a67c
Shashwat2104 <shashwatmahender2104@gmail.com>
2023-12-02 17:55:47 +0530

    Redux Done

---
+++ src/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { legacy_createStore as createStore } from "redux";
+import reducer from "./Reducers";
+import { incrementNumber, decrementNumber } from "./Actions";
+
+// const store = createStore(reducer);
+
+export default function Counter() {
+  const [count, setCounter] = useState(0);
+
+  const unsubscribe = store.subscribe(() => {
+    setCounter(store.getState().count);
+  });
+
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button onClick={() => store.dispatch(incrementNumber())}>Like</button>
+      <button onClick={() => store.dispatch(decrementNumber())}>Unlike</button>
+    </div>
+  );
+}
